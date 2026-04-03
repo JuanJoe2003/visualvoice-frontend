eb7d9d6135352ec4aeb2a9c89949a44dceb39aad
Juan Joe Edattukaran <87256857+JuanJoe2003@users.noreply.github.com>
2026-04-03 13:16:38 +0530

    Remove Firebase config values for security
    Removed sensitive Firebase configuration values.

--- js/firebase.js
+++ js/firebase.js
@@ -16,13 +16,13 @@ import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 
 // ── PASTE YOUR FIREBASE CONFIG HERE ──────────────────────────
 const firebaseConfig = {
-  apiKey: "AIzaSyCIE7taV5BSdKaHp3i6onfrIpLdRvvu59Q",
-  authDomain: "asl-teaching-web.firebaseapp.com",
-  projectId: "asl-teaching-web",
-  storageBucket: "asl-teaching-web.appspot.com",
-  messagingSenderId: "949463143708",
-  appId: "1:949463143708:web:10d99792abb862e6e216d9",
-  measurementId: "G-DE4NRCYC7K"
+  apiKey:
+  authDomain:
+  projectId:
+  storageBucket:
+  messagingSenderId:
+  appId:
+  measurementId:
 };
 // ─────────────────────────────────────────────────────────────
 
